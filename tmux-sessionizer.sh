4b10e5665bafd749d9bcb554e2c65e4e7dafb9ea
Aryan <singharyan4477@gmail.com>
2025-06-21 18:38:21 +0530

    initial commit

---
+++ tmux-sessionizer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set your projects base directory
+# PROJECTS_DIR=~/Projects
+PROJECTS_DIR=~/.config
+
+# Use fzf to pick a project
+DIR=$(find "$PROJECTS_DIR" -mindepth 1 -maxdepth 1 -type d | fzf)
+
+# Exit if no directory selected
+[ -z "$DIR" ] && echo "No project selected." && exit 1
+
+# Generate a session name from directory name
+SESSION=$(basename "$DIR" | tr . _)
+
+# If session doesn't exist, create it
+tmux has-session -t "$SESSION" 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -d -s "$SESSION" -c "$DIR"
+
+  # Window 1: nvim
+  tmux send-keys -t "$SESSION:1" 'nvim' C-m
+
+  # Window 2: shell
+  tmux new-window -t "$SESSION:2" -c "$DIR"
+fi
+
+# Attach to tmux session
+tmux attach -t "$SESSION"
+
